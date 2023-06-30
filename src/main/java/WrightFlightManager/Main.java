028f9df68960a14a03c072729d78f81dd6b78ae6
Shanmukha Mundra <71537991+w1180sxm@users.noreply.github.com>
2023-06-30 18:19:02 -0400

    Update Main.java
    Added initial welcome screen

--- src/main/java/WrightFlightManager/Main.java
+++ src/main/java/WrightFlightManager/Main.java
@@ -1,9 +1,26 @@
 package WrightFlightManager;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         // Call to establish connection to DB
         // Call to inject dependencies for interfaces as objects
-        // Call to run the GUI
+        SwingUtilities.invokeLater(() -> {
+            new WelcomeScreen();
+        });
     }
 }
+
+public class WelcomeScreen extends JFrame {
+    public WelcomeScreen() {
+        setTitle("Welcome");
+        setSize(600, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        JLabel welcomeLabel = new JLabel("Welcome to Wright Flight Manager App");
+        welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(welcomeLabel);
+
+        setVisible(true);
+    }
