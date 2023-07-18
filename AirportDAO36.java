568e70b232d88487c8905fbe194307e38714eb51
Manasa <133788960+w699mxc@users.noreply.github.com>
2023-07-18 11:51:08 -0400

    Created a method to read entries from the airports file.

---
+++ AirportDAO36.java
@@ -0,0 +1,42 @@
+//Issue 36
+import WrightFlightManager.MODEL.Airport;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AirportDAO {
+    private static final String FILE_PATH = "Airports.psv";
+
+    public List<Airport> getAllAirports() {
+        List<Airport> airports = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split("\\|");
+                if (data.length == 2) {
+                    String airportID = data[0];
+                    String airportName = data[1];
+                    airports.add(new Airport(airportID, airportName));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return airports;
+    }
+	
+	 public void addAirport(Airport airport) {
+        try (FileWriter writer = new FileWriter(FILE_PATH, true)) {
+            String line = airport.getAirportID() + "|" + airport.getAirportName() + "\n";
+            writer.write(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
