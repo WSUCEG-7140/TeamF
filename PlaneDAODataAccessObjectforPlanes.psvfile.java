cb05d2d5360c31d10c6595b348deb8aa0779ed8d
Manasa <133788960+w699mxc@users.noreply.github.com>
2023-07-26 19:29:33 -0400

    Update PlaneDAODataAccessObjectforPlanes.psvfile.java

--- PlaneDAODataAccessObjectforPlanes.psvfile.java
+++ PlaneDAODataAccessObjectforPlanes.psvfile.java
@@ -1,8 +1,5 @@
-//Issue 39
-/**
-*@file planeDAODataAccessObjectforPlanes.psvfile
-*@brief This file contains the PlaneDAO class responsible for accessing plane data from a PSV file
-*/
+// Issue 39
+
 package WrightFlightManager.dao;
 
 import WrightFlightManager.MODEL.Plane;
@@ -13,21 +10,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-/**
-*@class PlaneDAO
-*@brief The PlaneDAO class provides methods to read plane data from a psv file
-*/
 
-public class PlaneDAODataAccessObjectforPlanes.psvfile {
-/**
-  *@brief The file path to the PSV file containing plane data.
-    */
+public class PlaneDAO {
     private static final String FILE_PATH = "Planes.psv";
-    /**
-     *@brief Retrieves a list of all the planes from the PSV file.
-     *@return A list of Plane Objects representing all the planes in the file.
-      */
 
+    /**
+     * Retrieves a list of all planes from the data store.
+     *
+     * This method reads the data from the "Planes.psv" file and converts it into a list of Plane objects.
+     * Each line in the file represents a plane and is formatted as follows:
+     * PlaneType|FirstClassSeatCapacity|CoachClassSeatCapacity
+     *
+     * @return A list of Plane objects representing all the planes stored in the file.
+     */
     public List<Plane> getAllPlanes() {
         List<Plane> planes = new ArrayList<>();
 
@@ -43,9 +38,11 @@ public class PlaneDAODataAccessObjectforPlanes.psvfile {
                 }
             }
         } catch (IOException e) {
+            // If there is an issue reading the file, the exception will be caught and printed.
             e.printStackTrace();
         }
 
         return planes;
     }
 }
+
