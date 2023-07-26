2ec251453ef48db2aeb97e8f239875317c0b5856
Manasa <133788960+w699mxc@users.noreply.github.com>
2023-07-26 18:44:15 -0400

    Update PlaneDAODataAccessObjectforPlanes.psvfile.java

--- PlaneDAODataAccessObjectforPlanes.psvfile.java
+++ PlaneDAODataAccessObjectforPlanes.psvfile.java
@@ -1,5 +1,8 @@
 //Issue 39
-
+/**
+*@file planeDAODataAccessObjectforPlanes.psvfile
+*@brief This file contains the PlaneDAO class responsible for accessing plane data from a PSV file
+*/
 package WrightFlightManager.dao;
 
 import WrightFlightManager.MODEL.Plane;
@@ -10,9 +13,20 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+/**
+*@class PlaneDAO
+*@brief The PlaneDAO class provides methods to read plane data from a psv file
+*/
 
 public class PlaneDAODataAccessObjectforPlanes.psvfile {
+/**
+  *@brief The file path to the PSV file containing plane data.
+    */
     private static final String FILE_PATH = "Planes.psv";
+    /**
+     *@brief Retrieves a list of all the planes from the PSV file.
+     *@return A list of Plane Objects representing all the planes in the file.
+      */
 
     public List<Plane> getAllPlanes() {
         List<Plane> planes = new ArrayList<>();
