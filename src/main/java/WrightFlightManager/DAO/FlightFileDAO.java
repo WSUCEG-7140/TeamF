68f5e4bd68bf42df5f2412d98c0e6c6926a8dd17
Brian Davis <thebrianjdavis@gmail.com>
2023-07-27 13:45:22 -0400

    Implemented getAllFlights() method

--- src/main/java/WrightFlightManager/DAO/FlightFileDAO.java
+++ src/main/java/WrightFlightManager/DAO/FlightFileDAO.java
@@ -1,23 +1,27 @@
 package WrightFlightManager.DAO;
 
 import WrightFlightManager.MODEL.Flight;
-import WrightFlightManager.MODEL.User;
+import WrightFlightManager.MODEL.Role;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 
 /**
  * @class FlightFileDAO
- * @brief Handles data access operations related to the Flight model using a file-based data storage system.
+ * @brief Handles data access operations related to the Flight model using
+a file-based data storage system.
  * <p>
- * The FlightFileDAO class implements the iFlightDAO interface to manage data related to the "Flight" entity
- * using a file-based storage system. It represents the Data Access Object (DAO) pattern in the context
+ * The FlightFileDAO class implements the iFlightDAO interface to manage
+data related to the "Flight" entity
+ * using a file-based storage system. It represents the Data Access Object
+(DAO) pattern in the context
  * of managing roles.
  * <p>
- * For more information about the Data Access Object (DAO) pattern, please refer to
+ * For more information about the Data Access Object (DAO) pattern, please
+refer to
  * the Wikipedia page:
  * @ref https://en.wikipedia.org/wiki/Data_access_object
  */
@@ -31,18 +35,22 @@ public class FlightFileDAO implements iFlightDAO {
     /**
      * @brief Default constructor for FlightFileDAO.
      * <p>
-     * This constructor sets the default path for the file storing flight data.
-     * The default path is "src/main/java/WrightFlightManager/FILES/Flights.psv".
+     * This constructor sets the default path for the file storing flight
+    data.
+     * The default path is
+    "src/main/java/WrightFlightManager/FILES/Flights.psv".
      * Linked Issue: #7 https://github.com/WSUCEG-7140/TeamF/issues/7
      */
     public FlightFileDAO() {
-        flightFileName = "src/main/java/WrightFlightManager/FILES/Flights.psv";
+        flightFileName =
+                "src/main/java/WrightFlightManager/FILES/Flights.psv";
     }
 
     /**
      * @brief Parameterized constructor for FlightFileDAO.
      * <p>
-     * This constructor allows specifying a custom file path for storing flight data.
+     * This constructor allows specifying a custom file path for storing
+    flight data.
      * Linked Issue: #7 https://github.com/WSUCEG-7140/TeamF/issues/7
      * @param flightFileName The custom file path for storing flight data.
      */
@@ -53,10 +61,12 @@ public class FlightFileDAO implements iFlightDAO {
     /**
      * @brief Adds a new flight to the data storage system.
      * <p>
-     * This method is responsible for adding a new flight to the data storage system.
+     * This method is responsible for adding a new flight to the data
+    storage system.
      * The flight information is obtained from the provided Flight object.
      * Linked Issue: #43 https://github.com/WSUCEG-7140/TeamF/issues/43
-     * @param newFlight The User object representing the new flight to be added.
+     * @param newFlight The User object representing the new flight to be
+    added.
      * @return True if the flight is successfully added, false otherwise.
      */
     @Override
@@ -67,25 +77,82 @@ public class FlightFileDAO implements iFlightDAO {
     /**
      * @brief Retrieves all flights from the data storage system.
      * <p>
-     * This method retrieves all flight records from the file-based data storage system and returns them in
-     * a HashMap where the key is the flight number (a String) and the value is the corresponding Flight object.
+     * This method retrieves all flight records from the file-based data
+    storage system and returns them in
+     * a HashMap where the key is the flight number (a String) and the
+    value is the corresponding Flight object.
      * Linked Issue: #44 https://github.com/WSUCEG-7140/TeamF/issues/44
-     * @return A HashMap<String, Flight> containing all flight records in the data storage system.
+     * @return A HashMap<String, Flight> containing all flight records in
+    the data storage system.
      */
     @Override
-    public HashMap<String, Flight> getAllFlights() {
-        return null;
+    public HashMap<String, Flight> getAllFlights(){
+        HashMap<String, Flight> allFlights = new HashMap<>();
+
+        try {
+            RoleFileDAO roleDAO = new RoleFileDAO();
+            HashMap<Integer, Role> allRoles = roleDAO.getAllRoles();
+            Path flightFilePath = Path.of(flightFileName);
+            Scanner fileRead = new Scanner(flightFilePath);
+            while (fileRead.hasNextLine()) {
+                String line = fileRead.nextLine();
+                String[] splitLine = line.split("\\|");
+                if (splitLine.length == 12) {
+                    String flightNumber = splitLine[0];
+                    String originAirport = splitLine[1];
+                    String destinationAirport = splitLine[2];
+                    String planeType = splitLine[3];
+                    Date plannedDeparture =
+                            parseDateFromString(splitLine[4]);
+                    Date plannedArrival =
+                            parseDateFromString(splitLine[5]);
+                    Date actualDeparture =
+                            parseDateFromString(splitLine[6]);
+                    Date actualArrival =
+                            parseDateFromString(splitLine[7]);
+                    String departureGate = splitLine[8];
+                    String arrivalGate = splitLine[9];
+                    int firstClassSeatsFilled =
+                            Integer.parseInt(splitLine[10]);
+                    int coachClassSeatsFilled =
+                            Integer.parseInt(splitLine[11]);
+                    allFlights.put(flightNumber, new Flight(
+                            flightNumber,
+                            originAirport,
+                            destinationAirport,
+                            planeType,
+                            plannedDeparture,
+                            plannedArrival,
+                            actualDeparture,
+                            actualArrival,
+                            departureGate,
+                            arrivalGate,
+                            firstClassSeatsFilled,
+                            coachClassSeatsFilled
+                    ));
+                }
+            }
+            fileRead.close();
+        } catch (IOException e) {
+            // Ignore
+        }
+        return allFlights;
     }
 
     /**
-     * @brief Updates an existing flight's information in the data storage system.
+     * @brief Updates an existing flight's information in the data storage
+    system.
      * <p>
-     * This method is responsible for updating an existing flight's information in the data storage system.
-     * The new flight information is obtained from the provided Flight object, which should have the same flight
+     * This method is responsible for updating an existing flight's
+    information in the data storage system.
+     * The new flight information is obtained from the provided Flight
+    object, which should have the same flight
      * number as the one to be updated.
      * Linked Issue: #45 https://github.com/WSUCEG-7140/TeamF/issues/45
-     * @param updatedFlight The Flight object representing the updated flight information.
-     * @return True if the flight information is successfully updated, false otherwise.
+     * @param updatedFlight The Flight object representing the updated
+    flight information.
+     * @return True if the flight information is successfully updated,
+    false otherwise.
      */
     @Override
     public boolean updateFlight(Flight updatedFlight) {
@@ -95,12 +162,16 @@ public class FlightFileDAO implements iFlightDAO {
     /**
      * @brief Deletes a flight from the data storage system.
      * <p>
-     * This method is used to delete a flight from the file-based data storage system. The flight to be deleted
-     * is identified by the provided Flight object, which should have the same flight number as the one to be
+     * This method is used to delete a flight from the file-based data
+    storage system. The flight to be deleted
+     * is identified by the provided Flight object, which should have the
+    same flight number as the one to be
      * deleted.
      * Linked Issue: #46 https://github.com/WSUCEG-7140/TeamF/issues/46
-     * @param flightToDelete The Flight object representing the flight to be deleted.
-     * @return True if the flight is successfully deleted, false otherwise.
+     * @param flightToDelete The Flight object representing the flight to
+    be deleted.
+     * @return True if the flight is successfully deleted, false
+    otherwise.
      */
     @Override
     public boolean deleteFlight(Flight flightToDelete) {
@@ -110,10 +181,13 @@ public class FlightFileDAO implements iFlightDAO {
     /**
      * @brief Helper method to check if a Flight is in the flights file.
      * <p>
-     * The isFlightInFile method is a helper method that takes a flight as a parameter to check if a that
-     * Flight is in the flights file by searching for a match of the flight number
+     * The isFlightInFile method is a helper method that takes a flight as
+    a parameter to check if a that
+     * Flight is in the flights file by searching for a match of the
+    flight number
      * Linked Issue: #43 https://github.com/WSUCEG-7140/TeamF/issues/43
-     * @param checkFlight This represents the Flight being searched for within the file
+     * @param checkFlight This represents the Flight being searched for
+    within the file
      * @return Returns true if the flight was found in the file
      */
     private boolean isFlightInFile(Flight checkFlight) {
@@ -121,16 +195,56 @@ public class FlightFileDAO implements iFlightDAO {
     }
 
     /**
-     * @brief Helper method to write over the file specified by the flightFileName field with a new list of
+     * @brief Helper method to write over the file specified by the
+    flightFileName field with a new list of
      * flights.
      * <p>
-     * The writeToFlightsFile is a helper method to write over the file specified by the flightFileName
+     * The writeToFlightsFile is a helper method to write over the file
+    specified by the flightFileName
      * field with a new list of flights.
      * Linked Issue: #43 https://github.com/WSUCEG-7140/TeamF/issues/43
-     * @param updatedFlights This represents the new list of flights to be written to the flights file
+     * @param updatedFlights This represents the new list of flights to be
+    written to the flights file
      * @return Returns true if successfully written to flights file
      */
-    private boolean writeToFlightsFile(HashMap<String, Flight> updatedFlights) {
+    private boolean writeToFlightsFile(HashMap<String, Flight>
+                                               updatedFlights) {
         return false;
     }
+
+    /**
+     * @brief Parses a date from a string representation.
+     * <p>
+     * This helper method takes a date string in the format "yyyy-MM-dd
+    HH:mm:ss" and converts it into a
+     * Date object. The expected input format for the date string is
+    "yyyy-MM-dd HH:mm:ss". If the input date
+     * string is empty or null, the method will return null.
+     * @param dateToParse The date string to be parsed in the format
+    "yyyy-MM-dd HH:mm:ss".
+     * @return A Date object representing the parsed date, or null if the
+    input date string is empty or null.
+     */
+    private Date parseDateFromString(String dateToParse) {
+
+        Date dateFromString = null;
+
+        if (dateToParse.length() >= 1) {
+            String[] splitByHyphen = dateToParse.split("-");
+            int year = Integer.parseInt(splitByHyphen[0]);
+            int month = Integer.parseInt(splitByHyphen[1]);
+            String[] stringBeforeSpace = splitByHyphen[2].split("\\s");
+            int day = Integer.parseInt(stringBeforeSpace[0]);
+            String[] splitByColon = stringBeforeSpace[1].split(":");
+            int hours = Integer.parseInt(splitByColon[0]);
+            int minutes = Integer.parseInt(splitByColon[1]);
+            int seconds = Integer.parseInt(splitByColon[2]);
+
+            dateFromString = new Date(year, month, day, hours, minutes,
+                    seconds);
+        }
+
+        return dateFromString;
+    }
+
 }
--- src/main/java/WrightFlightManager/DAO/iFlightDAO.java
+++ src/main/java/WrightFlightManager/DAO/iFlightDAO.java
@@ -6,21 +6,27 @@ import java.util.HashMap;
 
 /**
  * @interface iFlightDAO
- * @brief This interface defines the contract for managing flights using a data access object (DAO) approach.
+ * @brief This interface defines the contract for managing flights using a
+data access object (DAO) approach.
  *
- * The iFlightDAO interface provides a set of methods for performing CRUD (Create, Read, Update, Delete) operations
- * related to the "Flight" entity. Classes that implement this interface are expected to provide concrete
- * implementations for these methods, allowing interaction with a data storage system for managing flights.
+ * The iFlightDAO interface provides a set of methods for performing CRUD
+(Create, Read, Update, Delete) operations
+ * related to the "Flight" entity. Classes that implement this interface
+are expected to provide concrete
+ * implementations for these methods, allowing interaction with a data
+storage system for managing flights.
  */
 public interface iFlightDAO {
 
     /**
      * @brief Adds a new flight to the data storage.
      *
-     * This method adds a new flight to the data storage (e.g., a file or database) if the flight number is not
+     * This method adds a new flight to the data storage (e.g., a file or
+    database) if the flight number is not
      * already present in the data storage.
      *
-     * @param newFlight The Flight object representing the new flight to be added.
+     * @param newFlight The Flight object representing the new flight to
+    be added.
      * @return true if the flight was successfully added, false otherwise.
      */
     boolean addNewFlight(Flight newFlight);
@@ -28,10 +34,12 @@ public interface iFlightDAO {
     /**
      * @brief Retrieves all flights from the data storage.
      *
-     * This method retrieves all flights from the data storage and returns them as a HashMap, where the keys
+     * This method retrieves all flights from the data storage and returns
+    them as a HashMap, where the keys
      * are flight numbers (as Strings) and the values are Flight objects.
      *
-     * @return A HashMap containing all Flights, where the keys are flight numbers (as Strings) and the values
+     * @return A HashMap containing all Flights, where the keys are flight
+    numbers (as Strings) and the values
      * are Flight objects.
      */
     HashMap<String, Flight> getAllFlights();
@@ -39,23 +47,31 @@ public interface iFlightDAO {
     /**
      * @brief Updates an existing flight in the data storage.
      *
-     * This method updates an existing flight in the data storage based on the information provided in the
-     * updatedFlight parameter. The update is performed based on the flight's unique identifier (flight number).
+     * This method updates an existing flight in the data storage based on
+    the information provided in the
+     * updatedFlight parameter. The update is performed based on the
+    flight's unique identifier (flight number).
      *
-     * @param updatedFlight The Flight object representing the updated flight information.
-     * @return true if the flight was successfully updated, false otherwise.
+     * @param updatedFlight The Flight object representing the updated
+    flight information.
+     * @return true if the flight was successfully updated, false
+    otherwise.
      */
     boolean updateFlight(Flight updatedFlight);
 
     /**
      * @brief Deletes a flights from the data storage.
      *
-     * This method deletes the specified flight from the data storage based on the information provided
-     * in the flightToDelete parameter. The deletion is performed based on the flight's unique identifier
+     * This method deletes the specified flight from the data storage
+    based on the information provided
+     * in the flightToDelete parameter. The deletion is performed based on
+    the flight's unique identifier
      * (flight number).
      *
-     * @param flightToDelete The Role object representing the role to be deleted.
+     * @param flightToDelete The Role object representing the role to be
+    deleted.
      * @return true if the role was successfully deleted, false otherwise.
      */
     boolean deleteFlight(Flight flightToDelete);
 }
+
