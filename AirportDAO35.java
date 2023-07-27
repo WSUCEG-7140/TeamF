c6f5f4c4e1fbf62d0ef99e590662a3c5e69625e7
Manasa <133788960+w699mxc@users.noreply.github.com>
2023-07-26 20:27:50 -0400

    Update AirportDAO35.java

--- AirportDAO35.java
+++ AirportDAO35.java
@@ -1,4 +1,9 @@
 //Isue 35
+/**
+ * @file AirportDAO35.java
+ * @brief This file contains the implementation of the AirportDAO35 class.
+ */
+
 package WrightFlightManager.dao;
 
 import WrightFlightManager.MODEL.Airport;
@@ -9,9 +14,24 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AirportDAO35{
+/**
+ * @class AirportDAO35
+ * @brief This class handles the data access operations for Airport objects.
+ */
+public class AirportDAO35 {
+    /**
+     * @brief The file path to store the airport data.
+     */
     private static final String FILE_PATH = "Airports.psv";
 
+    /**
+     * @brief Adds a new airport entry to the data storage.
+     * @param airport The Airport object to be added.
+     * @details This function takes an Airport object, converts it into a string representation,
+     *          and appends it to the data storage file.
+     * @note The airport data is stored in a pipe-separated values (PSV) format: "airportID|airportName".
+     * @exception IOException If an I/O error occurs while writing the data to the file.
+     */
     public void addAirport(Airport airport) {
         try (FileWriter writer = new FileWriter(FILE_PATH, true)) {
             String line = airport.getAirportID() + "|" + airport.getAirportName() + "\n";
@@ -21,3 +41,4 @@ public class AirportDAO35{
         }
     }
 }
+
