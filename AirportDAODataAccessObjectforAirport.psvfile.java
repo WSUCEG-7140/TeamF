0a49e1b91a1cb9943024e1767683d8af95e31969
Manasa <133788960+w699mxc@users.noreply.github.com>
2023-07-26 20:29:40 -0400

    Update AirportDAODataAccessObjectforAirport.psvfile.java

--- AirportDAODataAccessObjectforAirport.psvfile.java
+++ AirportDAODataAccessObjectforAirport.psvfile.java
@@ -1,4 +1,9 @@
 //Issue 34
+/**
+ * @file AirportDAODataAccessObjectforAirport.psvfile
+ * @brief This file contains the implementation of the AirportDAODataAccessObjectforAirport.psvfile class.
+ */
+
 package WrightFlightManager.dao;
 
 import WrightFlightManager.MODEL.Airport;
@@ -9,9 +14,23 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @class AirportDAODataAccessObjectforAirport.psvfile
+ * @brief This class provides data access operations for Airport objects using a pipe-separated values (PSV) file.
+ */
 public class AirportDAODataAccessObjectforAirport.psvfile {
+    /**
+     * @brief The file path to access the airport data.
+     */
     private static final String FILE_PATH = "Airports.psv";
 
+    /**
+     * @brief Retrieves a list of all airports stored in the data file.
+     * @return A List of Airport objects containing all airports in the data file.
+     * @details This function reads the data from the PSV file and converts it into a list of Airport objects.
+     *          Each line in the file is expected to be in the format: "airportID|airportName".
+     * @exception IOException If an I/O error occurs while reading the data from the file.
+     */
     public List<Airport> getAllAirports() {
         List<Airport> airports = new ArrayList<>();
 
